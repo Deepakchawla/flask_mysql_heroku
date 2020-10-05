35b41c41abbae0a4c181dab117b725d9de4bb87b
Deepak chawla <deepakchawla35@gmail.com>
2020-10-05 21:07:52 +0530

    update code

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+#from flask.sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://b5xt55al5mp52ni8:ww84p1dfvw33t2ky@k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/akodhyt06164v5xt'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://b5xt55al5mp52ni8:ww84p1dfvw33t2ky@k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/akodhyt06164v5xt'
 CORS(app, support_credentials=True)
 
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
 
 if __name__ == "__main__":
 
-    from views import *
+#    from views import *
     app.run(debug=True)
